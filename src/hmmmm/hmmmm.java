1a584237f0b7876042a66534871159750108c236
MaxBaldino <43002640+MaxBaldino@users.noreply.github.com>
2018-10-18 15:26:21 -0400

    Add files via upload

---
+++ src/hmmmm/hmmmm.java
@@ -0,0 +1,17 @@
+package hmmmm;
+
+import java.util.Scanner;
+
+public class hmmmm {
+
+	public static void main(String []args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter the amount of money in dollars:");
+		double startingMoney = Double.parseDouble(input.nextLine());
+		double doubleCash = (0.03 * startingMoney) * year;
+		System.out.println("The time it will take to double the money is");
+		
+	}
+	
+}
